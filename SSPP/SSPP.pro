121dfb4211b8613ffbd9031ed56a27ee9660fcfc
Juan Carlos Herrera Villaraus <juanvillaraus@gmail.com>
2016-03-07 13:10:58 -0600

    subi todo lo que se a hecho

---
+++ SSPP/SSPP.pro
@@ -0,0 +1,44 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-06-10T09:06:00
+# This software its developer for the Project SIVISO
+#
+#-------------------------------------------------
+
+QT       += core gui sql
+QT       += network
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = SSPP
+TEMPLATE = app
+
+BASEDIR = $$IN_PWD
+TARGETDIR = $$OUT_PWD
+BUILDDIR= $$TARGETDIR/build
+LANGUAGE=C++
+OBJECTS_DIR = $$BUILDDIR/obj
+MOC_DIR= $$BUILDDIR/moc
+UI_HEADERS_DIR = src/ui/generated
+
+
+SOURCES += main.cpp\
+        siviso.cpp \
+    blanco.cpp \
+    ppi.cpp \
+    signal.cpp \
+    dbasepostgresql.cpp \
+    wconfig.cpp
+
+HEADERS  += siviso.h \
+    blanco.h \
+    ppi.h \
+    signal.h \
+    dbasepostgresql.h \
+    wconfig.h
+
+FORMS    += siviso.ui \
+    wconfig.ui
+
+RESOURCES += \
+    resource.qrc
---
+++ build-SSPP-Desktop-Debug/build/moc/moc_siviso.cpp
@@ -0,0 +1,206 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'siviso.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../SSPP/siviso.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'siviso.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.2.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_siviso_t {
+    QByteArrayData data[25];
+    char stringdata[488];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_siviso_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_siviso_t qt_meta_stringdata_siviso = {
+    {
+QT_MOC_LITERAL(0, 0, 6),
+QT_MOC_LITERAL(1, 7, 10),
+QT_MOC_LITERAL(2, 18, 0),
+QT_MOC_LITERAL(3, 19, 21),
+QT_MOC_LITERAL(4, 41, 13),
+QT_MOC_LITERAL(5, 55, 13),
+QT_MOC_LITERAL(6, 69, 13),
+QT_MOC_LITERAL(7, 83, 14),
+QT_MOC_LITERAL(8, 98, 23),
+QT_MOC_LITERAL(9, 122, 24),
+QT_MOC_LITERAL(10, 147, 18),
+QT_MOC_LITERAL(11, 166, 4),
+QT_MOC_LITERAL(12, 171, 21),
+QT_MOC_LITERAL(13, 193, 18),
+QT_MOC_LITERAL(14, 212, 23),
+QT_MOC_LITERAL(15, 236, 26),
+QT_MOC_LITERAL(16, 263, 30),
+QT_MOC_LITERAL(17, 294, 26),
+QT_MOC_LITERAL(18, 321, 41),
+QT_MOC_LITERAL(19, 363, 23),
+QT_MOC_LITERAL(20, 387, 26),
+QT_MOC_LITERAL(21, 414, 5),
+QT_MOC_LITERAL(22, 420, 18),
+QT_MOC_LITERAL(23, 439, 23),
+QT_MOC_LITERAL(24, 463, 23)
+    },
+    "siviso\0leerSocket\0\0on_tipo_norte_clicked\0"
+    "on_nb_clicked\0on_bb_clicked\0on_wf_clicked\0"
+    "on_ppi_clicked\0on_origen_buque_clicked\0"
+    "on_origen_target_clicked\0on_bw_valueChanged\0"
+    "arg1\0on_pushButton_clicked\0"
+    "on_it_valueChanged\0on_edo_mar_valueChanged\0"
+    "on_prob_falsa_valueChanged\0"
+    "on_prob_deteccion_valueChanged\0"
+    "on_escala_ppi_valueChanged\0"
+    "on_escala_despliegue_tactico_valueChanged\0"
+    "on_gan_sen_valueChanged\0"
+    "on_frecuencia_valueChanged\0value\0"
+    "on_dt_valueChanged\0on_pushButton_2_clicked\0"
+    "on_pushButton_3_clicked\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_siviso[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      22,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  124,    2, 0x0a,
+       3,    0,  125,    2, 0x08,
+       4,    0,  126,    2, 0x08,
+       5,    0,  127,    2, 0x08,
+       6,    0,  128,    2, 0x08,
+       7,    0,  129,    2, 0x08,
+       8,    0,  130,    2, 0x08,
+       9,    0,  131,    2, 0x08,
+      10,    1,  132,    2, 0x08,
+      12,    0,  135,    2, 0x08,
+      13,    1,  136,    2, 0x08,
+      14,    1,  139,    2, 0x08,
+      15,    1,  142,    2, 0x08,
+      16,    1,  145,    2, 0x08,
+      17,    1,  148,    2, 0x08,
+      18,    1,  151,    2, 0x08,
+      19,    1,  154,    2, 0x08,
+      20,    1,  157,    2, 0x08,
+      22,    1,  160,    2, 0x08,
+      23,    0,  163,    2, 0x08,
+      24,    0,  164,    2, 0x08,
+      13,    1,  165,    2, 0x08,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Double,   11,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Double,   11,
+    QMetaType::Void, QMetaType::Int,   11,
+    QMetaType::Void, QMetaType::Double,   11,
+    QMetaType::Void, QMetaType::Double,   11,
+    QMetaType::Void, QMetaType::Double,   11,
+    QMetaType::Void, QMetaType::Double,   11,
+    QMetaType::Void, QMetaType::Int,   11,
+    QMetaType::Void, QMetaType::Int,   21,
+    QMetaType::Void, QMetaType::Double,   11,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   11,
+
+       0        // eod
+};
+
+void siviso::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        siviso *_t = static_cast<siviso *>(_o);
+        switch (_id) {
+        case 0: _t->leerSocket(); break;
+        case 1: _t->on_tipo_norte_clicked(); break;
+        case 2: _t->on_nb_clicked(); break;
+        case 3: _t->on_bb_clicked(); break;
+        case 4: _t->on_wf_clicked(); break;
+        case 5: _t->on_ppi_clicked(); break;
+        case 6: _t->on_origen_buque_clicked(); break;
+        case 7: _t->on_origen_target_clicked(); break;
+        case 8: _t->on_bw_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 9: _t->on_pushButton_clicked(); break;
+        case 10: _t->on_it_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 11: _t->on_edo_mar_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->on_prob_falsa_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 13: _t->on_prob_deteccion_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 14: _t->on_escala_ppi_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 15: _t->on_escala_despliegue_tactico_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 16: _t->on_gan_sen_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 17: _t->on_frecuencia_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 18: _t->on_dt_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 19: _t->on_pushButton_2_clicked(); break;
+        case 20: _t->on_pushButton_3_clicked(); break;
+        case 21: _t->on_it_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject siviso::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_siviso.data,
+      qt_meta_data_siviso,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *siviso::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *siviso::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_siviso.stringdata))
+        return static_cast<void*>(const_cast< siviso*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int siviso::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 22)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 22;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 22)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 22;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
